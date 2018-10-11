acd78cb73a33094245dd099a25e68e03b6626995
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-11 23:50:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,9 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) ||= self.create_by_name(name)
+    song = self.find_by_name(name)
+    return song if song
+    self.create_by_name(name)
   end
 
 end
